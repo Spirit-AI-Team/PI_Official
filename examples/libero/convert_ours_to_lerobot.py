3edc5cfc9399134985c79144cae0c079dfadc6f0
dylan wu <dylanwu711@gmail.com>
2025-03-03 17:02:42 +0800

    add visualize episode data

--- examples/libero/convert_ours_to_lerobot.py
+++ examples/libero/convert_ours_to_lerobot.py
@@ -30,6 +30,8 @@ import torch
 import tqdm
 import numpy as np
 import time
+import cv2
+import ffmpeg
 
 '''
 uv run examples/libero/convert_ours_to_lerobot.py --data-dir /hy-tmp/lmz/pi0_data/example --create-from-scratch
@@ -39,7 +41,7 @@ create-from-scratch: create lerobot dataset from scratch. this will Clean up any
 POOL_SIZE = 9
 LEFT_GRIPPER = 6
 RIGHT_GRIPPER = 13 
-REPO_NAME = "FlattenShirtNew"  # Name of the output dataset, also used for the Hugging Face Hub
+REPO_NAME = "temp"  # Name of the output dataset, also used for the Hugging Face Hub
 DATASET_TASK = {
 # '/pfstem/likaiyu/resources/hdf5/0_1new/20250225_Y_AL02_DYF03_PI0STEP01FINE_CXJ_ai_hdf5':'Flatten the shirt',
 '/pfstem/likaiyu/resources/hdf5/0_1new/20250225_Y_AL03_DYF03_PI0STEPS01_WHJ_ai_hdf5':'Flatten the shirt',
@@ -49,6 +51,41 @@ DATASET_TASK = {
 '/pfstem/likaiyu/resources/hdf5/0_1new/20250225_Y_AL08_DYF03_PI0STEPS01_LTJ_ai_hdf5':'Flatten the shirt',
 # '/pfstem/likaiyu/resources/hdf5/0_1new/20250225_Y_AL09_DYF03_PIOSTEP01FINE_SZH_ai_hdf5':'Flatten the shirt',
 }
+
+def vidwrite(filename, images, framerate=10, vcodec='libx264'):
+    """
+    Writes a sequence of NumPy arrays as a video file.
+
+    Args:
+        filename (str): The name of the output video file.
+        images (np.ndarray): A 4D NumPy array of shape (num_frames, height, width, channels).
+        framerate (int, optional): The frame rate of the video. Defaults to 30.
+        vcodec (str, optional): The video codec to use. Defaults to 'libx264'.
+    """
+    if not isinstance(images, np.ndarray):
+        images = np.asarray(images)
+    
+    n, height, width, channels = images.shape
+    process = (
+        ffmpeg
+        .input('pipe:', format='rawvideo', pix_fmt='rgb24', s='{}x{}'.format(width, height))
+        .output(filename, pix_fmt='yuv420p', vcodec=vcodec, r=framerate)
+        .overwrite_output()
+        .run_async(pipe_stdin=True)
+    )
+    for frame in images:
+        process.stdin.write(frame.astype(np.uint8).tobytes())
+    process.stdin.close()
+    process.wait()
+
+
+def draw_line_on_images(image, points, color=(81, 55, 255)):
+    num_frames = len(points)
+    if num_frames < 2:
+        cv2.circle(image, (points[0, 0, 0], points[0, 0, 1]), radius=2, color=color, thickness=2)
+    else:
+        cv2.polylines(image, [points], False, color, 2)
+    return image
 
 def main(data_dir: str = '/pfstem/likaiyu/resources/hdf5', *, 
          push_to_hub: bool = False, 
@@ -102,6 +139,13 @@ def main(data_dir: str = '/pfstem/likaiyu/resources/hdf5', *,
     else:
         dataset = LeRobotDataset(repo_id=REPO_NAME, local_files_only=True)
 
+    # visualize config
+    output_video_dir = os.path.join(LEROBOT_HOME, REPO_NAME, 'visualization')
+    os.system(f'mkdir -p {output_video_dir}')
+    total_visualize_episodes = 20
+    total_number_of_tasks = len(DATASET_TASK)
+    number_of_visualize_per_task = total_visualize_episodes // total_number_of_tasks
+
     # Loop over raw Libero datasets and write episodes to the LeRobot dataset
     # You can modify this for your own data format
     # H5 Action Format:
@@ -115,11 +159,13 @@ def main(data_dir: str = '/pfstem/likaiyu/resources/hdf5', *,
     #   l_joints l_grip  r_joints  r_grip
     
     for raw_dataset_name, task_instruction in DATASET_TASK.items():
-        # raw_dataset_name, task_instruction = pair    
+        # raw_dataset_name, task_instruction = pair 
+        cur_dataset_name = raw_dataset_name.split('/')[-1]   
         hdf5s_path = os.path.join(data_dir, raw_dataset_name)
         hdf5_file_names = os.listdir(hdf5s_path)
         hdf5_file_names.sort()
-        for hdf5_file_name in tqdm.tqdm(hdf5_file_names, total=len(hdf5_file_names)):
+        episode_index_to_viz = np.random.choice(len(hdf5_file_names), size=number_of_visualize_per_task)
+        for epi_idx, hdf5_file_name in tqdm.tqdm(enumerate(hdf5_file_names), total=len(hdf5_file_names)):
             hdf5_file_path = os.path.join(hdf5s_path, hdf5_file_name)
             mapping = {"observation.images.cam_high": 'camera0_rgb', "observation.images.cam_left_wrist": 'camera1_rgb', "observation.images.cam_right_wrist": 'camera2_rgb', "observation.state": ['robot0_gripper_width', 'robot1_gripper_width', 'robot_rjoint_rot_axis_angle'], "actions": ['robot0_gripper_width', 'robot1_gripper_width', 'robot_rjoint_rot_axis_angle']}
             value_dict = {"observation.images.cam_high": None, "observation.images.cam_left_wrist": None, "observation.images.cam_right_wrist": None, "observation.state": None, "actions": None}
@@ -164,6 +210,39 @@ def main(data_dir: str = '/pfstem/likaiyu/resources/hdf5', *,
                         }
                     )
                 dataset.save_episode(task=task_instruction)
+
+                # visualize data
+                # import ipdb; ipdb.set_trace()
+                if epi_idx in episode_index_to_viz:
+                    gripper_widths_images = None
+                    full_images = []
+                    total_number_of_frames = len(value_dict['observation.images.cam_left_wrist'])
+                    for frame_idx in range(total_number_of_frames):
+                        camera0_rgb = value_dict['observation.images.cam_high'][frame_idx]
+                        camera1_rgb = value_dict['observation.images.cam_left_wrist'][frame_idx]
+                        camera2_rgb = value_dict['observation.images.cam_right_wrist'][frame_idx]
+                        camera_images = np.concatenate([camera1_rgb, camera0_rgb, camera2_rgb], axis=1)
+
+                        height, width, channel = camera_images.shape
+
+                        gripper_image_height = 50
+                        gripper_widths_images = 255 * np.ones((gripper_image_height, width, channel))
+                        robot0_gripper_widths = gripper_image_height * value_dict['actions'][:frame_idx+1, 6] / 5.0
+                        robot1_gripper_widths = gripper_image_height * value_dict['actions'][:frame_idx+1, 13] / 5.0
+                        coords_x = width * 1.0 * np.arange(frame_idx + 1) / (total_number_of_frames)
+
+                        robot0_points = np.stack([coords_x, robot0_gripper_widths], axis=-1).reshape((-1, 1, 2)).astype(np.int32)
+                        robot1_points = np.stack([coords_x, robot1_gripper_widths], axis=-1).reshape((-1, 1, 2)).astype(np.int32)
+
+                        gripper_widths_images = draw_line_on_images(gripper_widths_images, robot0_points, color=(81, 55, 255))
+                        gripper_widths_images = draw_line_on_images(gripper_widths_images, robot1_points, color=(107, 255, 51))
+
+                        full_image = np.concatenate([camera_images, gripper_widths_images], axis=0)
+                        full_images.append(full_image)
+
+                    output_video_path = os.path.join(output_video_dir, f"{cur_dataset_name}_episode_{epi_idx}.mp4")
+                    vidwrite(output_video_path, full_images, framerate=10)
+
 
     # pool = ThreadPool(POOL_SIZE)
     # pool.map(func, [(k,v) for k,v in DATASET_TASK.items()]) 
