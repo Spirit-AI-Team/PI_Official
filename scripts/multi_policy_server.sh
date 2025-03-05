0eb9f243e43929f65083236b9081e7acb30feeba
Your Name <you@example.com>
2025-03-05 15:30:22 +0800

    policy switch test

--- scripts/multi_policy_server.sh
+++ scripts/multi_policy_server.sh
@@ -1,12 +1,12 @@
 
 export XLA_PYTHON_CLIENT_PREALLOCATE=false
-export CUDA_VISIBLE_DEVICES=5
-POLICY1_CONFIG=spi0_aloha_finetune_full_15steps
-POLICY1_DIR=/pfstem/wenxuan/resources/checkpoints/spi0_aloha_finetune_full_15steps/spi0_aloha_15steps_ftfull_0304/10000
+export CUDA_VISIBLE_DEVICES=0
+POLICY1_CONFIG=spi0_aloha_finetune_full
+POLICY1_DIR=/home/spirit-ai/codebase/PI_Official/checkpoints/shirtflatten_0225_27_bases2_full/29999
 POLICY1_PROMPT="Flatten the shirt"
 
-POLICY2_CONFIG=spi0_aloha_finetune_lora_15steps
-POLICY2_DIR=/pfstem/wenxuan/resources/checkpoints/spi0_aloha_finetune_lora_15steps/spi0_aloha_15steps_ftlora_0304/10000
+POLICY2_CONFIG=spi0_aloha_finetune_full_15steps
+POLICY2_DIR=/home/spirit-ai/codebase/PI_Official/checkpoints/spi0_aloha_15steps_quick_ftfull_0305/20000
 POLICY2_PROMPT="Fold the shirt"
 
 python scripts/serve_multipolicy.py --default_prompt1="${POLICY1_PROMPT}" --default_prompt2="${POLICY2_PROMPT}" policy1:checkpoint --policy1.config=$POLICY1_CONFIG --policy1.dir=$POLICY1_DIR  policy2:checkpoint --policy2.config=$POLICY2_CONFIG --policy2.dir=$POLICY2_DIR 
--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -497,13 +497,52 @@ _CONFIGS = [
         model=pi0.Pi0Config(action_horizon=25),
         exp_name = 'test',
         data=LeRobotAlohaDataConfig(
-            repo_id="FoldTheShirt15Steps",
+            repo_id="FlattenShirtNew25_27",
             assets=AssetsConfig(
                 assets_dir="assets/spi0_aloha_finetune_full",
-                asset_id="FoldTheShirt15Steps",
+                asset_id="FlattenShirtNew25_27",
             ),
             adapt_to_pi=True,
             default_prompt="fold the shirt",
+            repack_transforms=_transforms.Group(
+                inputs=[
+                    _transforms.RepackTransform(
+                        {
+                            "images": {
+                                "cam_high": "observation.images.cam_high",
+                                "cam_left_wrist": "observation.images.cam_left_wrist",
+                                "cam_right_wrist": "observation.images.cam_right_wrist",
+                            },
+                            "state": "observation.state",
+                            "actions": "actions",
+                        }
+                    )
+                ]
+            ),
+            base_config=DataConfig(
+                local_files_only=True,  # Set to True for local-only datasets.
+            ),
+        ),
+        batch_size=32,
+        num_workers=4,
+        fsdp_devices=2,
+        weight_loader=weight_loaders.CheckpointWeightLoader("checkpoints/shirtflatten_0225_base0223/29999/params"),
+        num_train_steps=30_000,
+        lr_schedule = _optimizer.CosineDecaySchedule(decay_steps=30_000),
+        checkpoint_base_dir="/pfstem/likaiyu/resources/checkpoints",
+    ),
+    TrainConfig(
+        name="spi0_aloha_finetune_full_15steps",
+        model=pi0.Pi0Config(action_horizon=25),
+        exp_name = 'test',
+        data=LeRobotAlohaDataConfig(
+            repo_id="FoldTheShirt15StepsQuick",
+            assets=AssetsConfig(
+                assets_dir="assets/spi0_aloha_finetune_full_15steps",
+                asset_id="FoldTheShirt15StepsQuick",
+            ),
+            adapt_to_pi=True,
+            default_prompt="Fold the shirt",
             repack_transforms=_transforms.Group(
                 inputs=[
                     _transforms.RepackTransform(
