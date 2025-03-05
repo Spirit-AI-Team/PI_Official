4a13d8ea2737e0f073907656d5bba15e814df927
dylan.wu <dylanwu711@outlook.com>
2025-03-05 11:05:33 +0800

    fix command

--- scripts/multi_policy_server.sh
+++ scripts/multi_policy_server.sh
@@ -1,4 +1,4 @@
-cd /pfstem/wenxuan/code/PI_Official
+
 export XLA_PYTHON_CLIENT_PREALLOCATE=false
 export CUDA_VISIBLE_DEVICES=5
 POLICY1_CONFIG=spi0_aloha_finetune_full_15steps
@@ -7,7 +7,7 @@ POLICY1_DIR=/pfstem/wenxuan/resources/checkpoints/spi0_aloha_finetune_full_15ste
 POLICY2_CONFIG=spi0_aloha_finetune_lora_15steps
 POLICY2_DIR=/pfstem/wenxuan/resources/checkpoints/spi0_aloha_finetune_lora_15steps/spi0_aloha_15steps_ftlora_0304/10000
 
-python scripts/serve_policy.py policy1:checkpoint --policy1.config=$POLICY1_CONFIG --policy1.dir=$POLICY1_DIR policy2:checkpoint --policy2.config=$POLICY2_CONFIG --policy2.dir=$POLICY2_DIR
+python scripts/serve_multipolicy.py policy1:checkpoint --policy1.config=$POLICY1_CONFIG --policy1.dir=$POLICY1_DIR policy2:checkpoint --policy2.config=$POLICY2_CONFIG --policy2.dir=$POLICY2_DIR
 
 
 
