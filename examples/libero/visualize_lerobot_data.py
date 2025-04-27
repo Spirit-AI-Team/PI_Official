a7728d49464c451656904b33075af0cd09b190e0
yklInverted <greencake.lky@gmail.com>
2025-04-28 04:05:15 +0800

    train stick on tissue 0427

--- examples/libero/visualize_lerobot_data.py
+++ examples/libero/visualize_lerobot_data.py
@@ -12,7 +12,7 @@ import ffmpeg
 from pathlib import Path
 
 # set the LEROBOT_HOME and REPO_NAME to the dataset you need.
-REPO_NAME = "MultiTask_EggInEggrack_0414"
+REPO_NAME = "MultiTask_PutAnimalInOrganizer_0414"
 
 def vidwrite(filename, images, framerate=10, vcodec='libx264'):
     """
