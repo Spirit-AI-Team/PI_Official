85c06c6a5c0f14bc197d82f760d1dd2a99d3d04b
yklInverted <greencake.lky@gmail.com>
2025-05-04 09:40:09 +0800

    train 10 tasks

--- scripts/train1.sh
+++ scripts/train1.sh
@@ -2,18 +2,18 @@ CONFIG=spi0_aloha_eef_multi_full
 # TASK=shirt_pretrains2_0225
 # TASK=FlattenShirt_EEF_0306_11_debug
 # TASK=MultiTask_6Objs_0331_03_pi0base
-TASK=PlateOnRack_EEF_HRPI_0424
-CKPT=29999
+TASK=MultiTask_10task_0427
+CKPT=59999
 
 cd /root/PI_Official
 source .venv/bin/activate
 export XDG_CACHE_HOME=/pfstem/likaiyu/resources/.cache
 
 #compute norm
-# CUDA_VISIBLE_DEVICES=0 python scripts/compute_norm_stats_multi.py --config_name $CONFIG --max_frames 10000
+CUDA_VISIBLE_DEVICES=0 python scripts/compute_norm_stats_multi.py --config_name $CONFIG --max_frames 20000
 
 #train model
-CUDA_VISIBLE_DEVICES=0,1 python scripts/train.py $CONFIG --exp-name=$TASK --resume
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python scripts/train.py $CONFIG --exp-name=$TASK --resume
 
 #zip ckpt
 cd /pfstem/likaiyu/resources/checkpoints/$CONFIG/$TASK
